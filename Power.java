7b4f2e717752000cf640f719164361c61511bf11
Indumathi575 <indusri2004@gmail.com>
2024-07-01 22:09:52 +0530

    program using power

---
+++ Power.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Power
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int a,b;
+
+a=s.nextInt();
+b=s.nextInt();
+double c=(int)Math.pow(a,b);
+System.out.println(c);
+
+
+}
+}
