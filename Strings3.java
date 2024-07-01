a6935ea855e237af396a634417a9183e8dd758d3
Indumathi575 <indusri2004@gmail.com>
2024-07-01 22:15:13 +0530

    Add files via upload

---
+++ Strings3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Strings4
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+ String s1="Java Lang";
+System.out.println(s1);
+char s2[]=s1.tocharArray();
+System.out.println(s2));
+
+}
+}
