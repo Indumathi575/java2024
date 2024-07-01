dc6ba40cad4d3c11545ec504f13afd89d5e7da47
Indumathi575 <indusri2004@gmail.com>
2024-07-01 22:15:36 +0530

    Add files via upload

---
+++ Strings4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Strings5
+{
+public static void main(String args[])
+{
+String s1="Java is a simple Language";
+System.out.println(s1.replace("simple","dificult");
+System.out.println(s1.indexOf("simple"));
+System.out.println(s1.indexOf("i");
+System.out.println(s1.lastIndexOf("i"));
+}
+}
