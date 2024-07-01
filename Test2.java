99ee02f4720b43d299286a95c3f428c651ae257c
Indumathi575 <indusri2004@gmail.com>
2024-07-01 22:20:29 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,13 @@
+class Test2
+{
+public static void main(String[] args)
+{
+int a,b,c;
+a=75;
+b=82;
+c=a+b;
+System.out.println("Sum="+c);
+System.out.printf("value of a="+a+"value of b="+b+"sum="+c);
+
+}
+}
