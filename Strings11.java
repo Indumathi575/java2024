9d17bd7e6e6f3bdbe64717616875e8a16fe420b0
Indumathi575 <indusri2004@gmail.com>
2024-07-02 15:39:37 +0530

    String

---
+++ Strings11.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Strings11
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str1=s.next();
+String str2=s.next();
+if(str1.contains(str2))
+{
+System.out.println("found");
+}
+else
+{
+System.out.println("not found");
+}
+}
+}
+
+
+
+
+
+
+
+
