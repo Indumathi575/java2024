21a02f84568451675a6dd734fa6c6a3b1369d058
Indumathi575 <indusri2004@gmail.com>
2024-07-02 14:58:38 +0530

    strings

---
+++ LowerToUpper.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class LowerToUpper
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str="Java is a simple language";
+System.out.println(str.toLowerCase());
+System.out.println(str.toUpperCase());
+}
+}
