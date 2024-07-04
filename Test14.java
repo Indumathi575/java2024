b599fefb4c1784976ce0723f7a2a9c99e76d9cca
Indumathi575 <indusri2004@gmail.com>
2024-07-04 15:54:55 +0530

    Instance variables

---
+++ Test14.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Sample
+{
+int a;
+double d;
+boolean b;
+String s;
+}
+class Test14
+{
+public static void main(String args[])
+{
+Sample s1=new Sample();
+System.out.println(s1.a);
+System.out.println(s1.d);
+System.out.println(s1.b);
+System.out.println(s1.s);
+
+}
+}
